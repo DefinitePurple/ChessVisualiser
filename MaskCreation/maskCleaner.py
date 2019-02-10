a55804a5ab7b6a4c65f22c241cffe51af72cde39
DefinitePurple <Danieljfitzpatrick3696@gmail.com>
2019-02-10 22:55:17 +0000

    Removing old images

---
+++ MaskCreation/maskCleaner.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+from matplotlib import pyplot as plt
+import glob
+
+DATA_DIR = '../Data'
+IMAGES_DIR = '/Images'
+imgs = os.listdir(DATA_DIR + IMAGES_DIR)
+imgs = [k for k in imgs if 'masks' in k and '.jpg' in k and '~' not in k]
+
+height = width = 600
+dim = (width, height)
+
+for img_path in imgs:
+    full_path = DATA_DIR + IMAGES_DIR + '/' + img_path
+    print(full_path)
+    img = cv2.imread(full_path)
+
+    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+    for i in range(0, 600):
+        for j in range(0, 600):
+            color = list(resized[i][j])
+            print(color)
+            if color is [0,0,0]:
+                print('next')
+            break
+        break
+
+    plt.imshow(resized)
+    plt.show()
+    # cv2.imwrite(DATA_DIR + IMAGES_DIR + '/COPY-' + img, resized)
